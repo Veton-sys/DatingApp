b8ab8dfd2bb451e2ae4244dc7a2bef4d077fafb4
vetonxhumkar890@outlook.com <vetonxhumkar890@outlook.com>
2023-08-04 14:14:42 +0200

    CommitFix

---
+++ API/Program.cs
@@ -0,0 +1,24 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options => 
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseCors(corsBuilder => corsBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.MapControllers();
+
+
+app.Run();
