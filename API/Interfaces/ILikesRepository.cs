37686944bd56fa5f12d1f46961880eb81ca21608
vetonxhumkar890@outlook.com <vetonxhumkar890@outlook.com>
2023-08-30 23:04:39 +0200

    End of section 14

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,13 @@
+using API.DTOs;
+using API.Entities;
+using API.Helpers;
+
+namespace API.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);
+        Task<AppUser> GetUserWithLikes(int userId);
+        Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams); //user they liked or users that liked them
+    }
+}
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -45,6 +45,7 @@ export class MembersService {
     }
     return;
   }
+
   getMembers(userParams: UserParams) {
     const response = this.memberCache.get(Object.values(userParams).join('-'));
 
@@ -90,6 +91,18 @@ export class MembersService {
 
   deletePhoto(photoId: number) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+  }
+
+  addLike(username: string){
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string, pageNumber: number, pageSize: number){
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+
+    params = params.append('predicate', predicate);
+
+    return this.getPaginatedResults<Member[]>(this.baseUrl + 'likes', params);
   }
 
   private getPaginatedResults<T>(url: string, params: HttpParams) {
